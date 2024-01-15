0ca10062e94c7ea81d0810a07a955db186406efd
acabrerrob <acabrerrob@educacion.navarra.es>
2024-01-15 20:09:01 +0100

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,37 +1,39 @@
+def fibonacciBucle(n):
+    '''
+    Función que recibe u número entero positivo n 
+    y calcula el número de fibonacci asociado a ese número.
+   
+    - Parámetro = n: número entero positivo.
+    - Salida: número de fibonacci asociado al parámetro n introducido.
+  
+    '''
+    a = 0
+    b = 1
+    
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        for index in range(n-1):
+            c = a + b
+            a = b
+            b = c
+        return c
+
+
+
 # Escribir dos funciones, una función que reciba un 
 # número entero positivo n y calcule el número de 
 # fibonacci asociado a ese número de manera recursiva 
 # y otra función que haga la misma operación pero con bucles.
 # Con ambas funciones, calcular y comparar el tiempo de 
 # ejecución para n = (1, 10, 20, 30 y 40) por fuerza bruta.
-def fibonacciBucle(n):
-    '''
-    
-    
-    '''
-    a = 0
-    b = 1
-    count = 2
 
-
+def fibonacciRecursive(n):
     if n == 0:
-        print('Su posición corresponde a: 0')
+        return 0
     elif n == 1:
-        print('Su posición corresponde a: 1')
-    else:
-        for i in range (n-1):
-            c = a + b
-            c <= n    
-            a = b
-            b = c
-            count += 1
-
-        print(f'Su posición corresponde a: {count}')
-
-
-
-
-
-
-        
-fibonacciBucle(1)
+        return 1
+    return fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
+print(fibonacciRecursive(3))
